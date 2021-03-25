b87dccfa6ebe3c31bc91b38b99f069beea8b7816
BEduardo <beduardo.dev@gmail.com>
2021-03-25 19:52:33 -0300

    Adicionando if e else encadeado

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -6,20 +6,26 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 );
 
-const idadeComprador = 15;
-const estaAcompanhada = true;
+const idadeComprador = 18;
+const estaAcompanhada = false;
+const temPassagemComprada = true;
 
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
-    listaDeDestinos.splice(1, 1);
-    console.log('Comprador maior idade!');
-} else if (estaAcompanhada) {
-    console.log("Comprador está acompanhado!");
-    listaDeDestinos.splice(1, 1);
+if (idadeComprador >= 18 || estaAcompanhada) {
+    listaDeDestinos.splice(2, 1);
+    console.log('Boa Viagem!');
 } else {
     console.log('Comprador não é maior idade!');
+}
+
+console.log("\nEmbarque:");
+
+if (idadeComprador >= 18 && temPassagemComprada) {
+    console.log("Boa Viagem!");
+} else {
+    console.log("Voce nao pode embarcar!");
 }
 
 console.log(listaDeDestinos);
